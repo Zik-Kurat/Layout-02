0fa0115bc7a9be47d647ef23f5afcddc52c2c186
Zikkurat <liholat.nikita.zikkurat@yandex.ru>
2025-02-26 23:25:33 +1100

    -

--- zk-modules/js/toggle-tool-kit-01.js
+++ zk-modules/js/toggle-tool-kit-01.js
@@ -2,26 +2,30 @@
 
 // Указанные ниже CSS-классы назначаются программистом.
 // .ttk01
-// .ttk01--button
-// .ttk01--area-button
 
 // Указанные ниже HTML-атрибуты назначается программистом.
-// data-ttk01-group
+// data-ttk01-id
 // data-ttk01-max-width
+// data-ttk01-method-<id>-<method>
+
+// <method>:
+// observer
+// button
+// area-button
 
 // Указанные ниже HTML-атрибуты назначается скриптом.
-// data-ttk01-flag
+// data-ttk01-<id>-flag
 
 // ---------------------------------------------------------------------
 
 const TTKs = document.querySelectorAll('.ttk01');
 TTKs.forEach(ttk => {
-    const GROUP_NAME = ttk.dataset.ttk01Group;
+    const ID = ttk.dataset.ttk01Id;
     const MAX_WIDTH = +ttk.dataset.ttk01MaxWidth;
 
-    const ALL          = document.querySelectorAll('*[data-ttk01-group="'                   + GROUP_NAME + '"]');
-    const BUTTONs      = document.querySelectorAll('.ttk01--button[data-ttk01-group="'      + GROUP_NAME + '"]');
-    const AREA_BUTTONs = document.querySelectorAll('.ttk01--area-button[data-ttk01-group="' + GROUP_NAME + '"]');
+    const OBSERVERs    = document.querySelectorAll('*[data-ttk01-method-' + ID + '-observer]');
+    const BUTTONs      = document.querySelectorAll('*[data-ttk01-method-' + ID + '-button]');
+    const AREA_BUTTONs = document.querySelectorAll('*[data-ttk01-method-' + ID + '-area-button]');
 
     let flag = false;
     setFlag(flag);
@@ -29,8 +33,11 @@ TTKs.forEach(ttk => {
     // ---------------------------------------------------------------------
 
     function setFlag(flag) {
-        ALL.forEach(element => {
-            element.dataset.ttk01Flag = flag;
+        OBSERVERs.forEach(element => {
+            element.setAttribute(
+                'data-ttk01-' + ID + '-flag',
+                flag
+            );
         });
     }
 
